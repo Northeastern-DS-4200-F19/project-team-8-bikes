66271099e16983435906c108fff630d8f071ecd7
JillianBurns <jillian.jb.burns@gmail.com>
2019-11-06 13:04:22 -0500

    fixed y locations for stacked bars

--- visualization.js
+++ visualization.js
@@ -1,43 +1,33 @@
 /* before document loads */
+// converts to lowercase & replaces spaces with '-'
+const formatStreetName = street => street.toLowerCase().replace(/\s/g, '-');
 
+// create one data object for each bike lane type on each street
+function formatData(data, bikeLaneTypes) {
+    let newData = [];
+
+    data.forEach((d, i) => {
+        let bikeLanes = [];
+        bikeLaneTypes.forEach((bl, j) => {
+            bikeLanes.push({
+                location: d["Location"],
+                neighborhood: d["Neighborhood"],
+                type: bl,
+                segments: d["Segments"],
+                percent: d[bl],
+                x: i,
+                y: j>0 ? Number(bikeLanes[j-1].y) + Number(bikeLanes[j-1].percent) : 0,
+            })
+        });
+        newData.push(bikeLanes);
+    });
+
+    return newData;
+}
 
 /* document loaded */
 $(function() {
     d3.csv("data/Street Segment Bike Lanes.csv").then(renderChart);
-
-    // create one data object for each bike lane type on each street
-    function formatData(data, bikeLaneTypes) {
-        let newData = [];
-
-        data.forEach((d, i) => {
-            let bikeLanes = [];
-            bikeLaneTypes.forEach((bl, j) => {
-                bikeLanes.push({
-                    location: d["Location"],
-                    neighborhood: d["Neighborhood"],
-                    type: bl,
-                    segments: d["Segments"],
-                    percent: d[bl],
-                    x: i,
-                    y: j,
-                })
-            });
-            newData.push(bikeLanes);
-            /*
-            bikeLaneTypes.forEach((bl, i) => {
-                newData.push({
-                    location: d["Location"],
-                    neighborhood: d["Neighborhood"],
-                    type: bl,
-                    segments: d["Segments"],
-                    percent: d[bl],
-                    y: i,
-                });
-            });*/
-        });
-
-        return newData;
-    }
 
     function renderChart(data) {
         let streets = data.map(d => d.location);
@@ -49,24 +39,24 @@ $(function() {
         // sources:
         //https://bl.ocks.org/KingOfCramers/04dcd9742a2be13d99db5f7a7480b4ca
         //http://bl.ocks.org/mstanaland/6100713
-        let svg = d3.select("body")
-            .append("svg")
+        let svg = d3.select("#vis-svg")
                 .attr("width", 550)
                 .attr("height", 500);
 
         let xScale = d3.scaleLinear()
-            .domain([0, data.length-1])
-            .range([20, 480]);
+            .domain([0, data.length])
+            .range([20, 480])
+            //.tickFormat((d,i) => streets[i])
 
         // Notice that the yScale is not inverted on the yScale
         let yScale = d3.scaleLinear()
             .domain([0, 100])
-            .range([480, 20]);
+            .range([480,20]);
 
         // We create a heightscale where the y is inverted!
         let heightScale = d3.scaleLinear()
             .domain([0, 100])
-            .range([20, 480]);
+            .range([0, 460]);
 
         let colorScale = ["#fcd88a", "#cf7c1c", "#93c464", "#75734F", "#5eafc6", "#41a368", "#41a368", "#41a368"];
 
@@ -74,14 +64,13 @@ $(function() {
             .scale(yScale)
             .tickSize(-500);
 
+        let xAxis = d3.axisBottom()
+            .scale(xScale);
+
         svg.append("g")
             .attr("class", "yAxis")
             .call(yAxis)
             .attr("transform",`translate(${20}, 0)`);
-
-        let xAxis = d3.axisBottom()
-            .scale(xScale)
-                .tickSize(-500);
 
         svg.append("g")
             .attr("class", "xAxis")
@@ -92,37 +81,34 @@ $(function() {
 
         d3.select("g.xAxis").selectAll("g.tick").selectAll("line");
 
-        let stackLayout = d3.stack()
-            .keys(streets);
-
         // Create groups for each series, rects for each segment
         let groups = svg.selectAll("g.bars")
             .data(lanes)
             .enter()
                 .append("g")
-                .attr("class", "bars")
+                .attr("class", d => `bars street-${formatStreetName(d[0].location)}`);
 
         // create bar rectangles
         let rect = groups.selectAll("rect")
             .data(d => d)
             .enter()
                 .append("rect")
-                    .attr("x", (d, i) => xScale(d.x+1)-10)
-                    .attr("y", (d, i) => yScale(d.percent - d.y))
-                    .attr("height", d => yScale(d.percent))
+                    .attr("x", d => xScale(d.x+1)-10)
+                    .attr("y", d => yScale(d.y)-heightScale(d.percent))
+                    .attr("height", d => heightScale(d.percent))
                     .attr("width", () => 25)
-                    .attr("class",
-                            d => `street-${d.location.toLowerCase().replace(/\s/g, '-')}`)
-                    .style("fill", (d, i) => colorScale[i]);
-            // for adding tooltips
-            /*.on("mouseover", () => tooltip.style("display", null))
-                .on("mouseout", () => tooltip.style("display", "none"))
-                .on("mousemove", d => {
-                    var xPosition = d3.mouse(this)[0] - 15;
-                    var yPosition = d3.mouse(this)[1] - 25;
-                    tooltip.attr("transform", "translate(" + xPosition + "," + yPosition + ")");
-                    tooltip.select("text").text(d.y);
-            })*/;
+                    .attr("class", d => `street-${formatStreetName(d.location)} lane-${d.type}`)
+                    .style("fill", (d, i) => colorScale[i])
+                    // for adding tooltips
+                    /*
+                    .on("mouseover", () => tooltip.style("display", null))
+                    .on("mouseout", () => tooltip.style("display", "none"))
+                    .on("mousemove", d => {
+                        var xPosition = d3.mouse(this)[0] - 15;
+                        var yPosition = d3.mouse(this)[1] - 25;
+                        tooltip.attr("transform", "translate(" + xPosition + "," + yPosition + ")");
+                        tooltip.select("text").text(d.y);
+                    })*/;
     }
 });
 
